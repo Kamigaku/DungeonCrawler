740b6476ca82179870e38284a74520771b5f4ffe
Kamigaku <Kamigaku>
2016-04-16 11:41:38 +0200

    Premier push
    Base du projet

---
+++ desktop/src/com/kamigaku/dungeoncrawler/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kamigaku.dungeoncrawler.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kamigaku.dungeoncrawler.GameLauncher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameLauncher(), config);
+	}
+}
